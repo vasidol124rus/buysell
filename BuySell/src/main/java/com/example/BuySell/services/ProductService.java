e9e1e3f5f17abebd0fd3c04f9f41ca75c65eb92d
vasidol123rus <vasekku25@gmail.com>
2025-03-05 19:01:27 +0700

    servis up

--- BuySell/src/main/java/com/example/BuySell/services/ProductService.java
+++ BuySell/src/main/java/com/example/BuySell/services/ProductService.java
@@ -6,26 +6,32 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class ProductService {
-    private List<Product> products =new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
     private long ID = 0;
 
     {
-        products.add(new Product(++ID,"PlayStation 5", "Simple Description", 45000, "Kiev", "vega"));
-        products.add(new Product(++ID,"Nokia 3310", "Simple Description", 4000, "Krasnoyarsk", "sega"));
-
+        products.add(new Product(++ID, "PlayStation 5", "Simple description", 67000, "Krasnoyarsk", "tomas"));
+        products.add(new Product(++ID, "Iphone 8", "Simple description", 24000, "Moscow", "artmcoder"));
     }
 
-    public List<Product> listProducts() {
-        return products;
-    }
-    public void saveProduct(Product product){
+    public List<Product> listProducts() { return products; }
+
+    public void saveProduct(Product product) {
         product.setId(++ID);
         products.add(product);
     }
-    public void deleteProduct(Long id){
-        products.removeIf(product -> product.getId() == id);
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
     }
 
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+        return null;
+    }
 }
