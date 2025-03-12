b3f53b690730cd3e4c47ec2ae4f1bdd3b05b1b07
vasidol123rus <vasekku25@gmail.com>
2025-03-12 15:38:01 +0700

    правка аботы user  c зкщвгсе

--- BuySell/src/main/java/com/example/BuySell/controller/UserController.java
+++ BuySell/src/main/java/com/example/BuySell/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.BuySell.models.User;
 import com.example.BuySell.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,8 +25,11 @@ public class UserController {
 
 
     @PostMapping("/registration")
-    public String createUser(User user) {
-        userService.createUser(user);
+    public String createUser(User user, Model model) {
+        if (!userService.createUser(user)) {
+            model.addAttribute("errorMessage", "Пользователь с email: " + user.getEmail() + " уже существует");
+            return "registration";
+        }
         return "redirect:/login";
     }
 
